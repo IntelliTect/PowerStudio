0b6d00f0f67455ef3d9a39dbcf6d325c966048bf
Ian Davis <ian@innovatian.com>
2011-03-27 12:59:51 -0700

    updating licensing in a few files.

--- source/Language/Scanner.cs
+++ source/Language/Scanner.cs
@@ -1,4 +1,15 @@
-﻿using System.Collections.Generic;
+﻿#region License
+
+// 
+// Copyright (c) 2011, PowerStudio Project Contributors
+// 
+// Dual-licensed under the Apache License, Version 2.0, and the Microsoft Public License (Ms-PL).
+// See the file LICENSE.txt for details.
+// 
+
+#endregion
+
+using System.Collections.Generic;
 
 namespace PowerStudio.Language
 {
