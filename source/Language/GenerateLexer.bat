d604428bbc4f602a8ae0da3de27491a68286d813
Ian Davis <ian@innovatian.com>
2011-04-16 07:40:14 -0700

    removing old language projects

--- source/Language/GenerateLexer.bat
+++
@@ -1,2 +0,0 @@
-"..\..\Tools\GPLEX\binaries\gplex.exe" /babel /unicode /utf8default /verbose /out:".\Scanner.Designer.cs" "lexer.lex"
-pause
--- source/Language/GenerateParser.bat
+++
@@ -1,2 +0,0 @@
-"..\..\Tools\GPPG\binaries\gppg.exe" /babel /gplex /verbose /report /out:".\Parser.Designer.cs" "parser.y"
-pause
