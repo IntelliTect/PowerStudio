b268e8ccd326da394ab410ed25efd80185817437
Ian Davis <idavis@innovatian.com>
2011-04-20 19:52:19 -0700

    adding unit testing to build script

--- build.ps1
+++ build.ps1
@@ -5,6 +5,28 @@
 # See the file LICENSE.txt for details.
 # 
 
+function Test-Is64Bit {
+	$ptrSize = [System.IntPtr]::Size
+	switch ($ptrSize) {
+		4 {
+			return $false
+		}
+		8 {
+			return $true
+		}
+		default {
+            throw ("Unknown pointer size ({0}) returned from System.IntPtr." -f $ptrSize)
+        }
+    }
+}
+
+function Get-ProgramFilesX86 {
+	if(Test-Is64Bit) {
+	    return (Get-Item "env:ProgramFiles(x86)").Value
+	}
+	return (Get-Item "env:ProgramFiles").Value
+}
+
 properties {
   $solution_name = "PowerStudio"
   $base_directory = resolve-path .
@@ -21,13 +43,18 @@ properties {
   $msbuild_logfile = 'MSBuildOutput.txt'
   $max_cpu_count = [System.Environment]::ProcessorCount / 2
   $build_in_parralel = $true
+  $mstest = [System.IO.Path]::Combine((Get-ProgramFilesX86), "Microsoft Visual Studio 10.0\Common7\IDE\MSTest.exe")
 }
 
 task build -depends Compile, Test
 task default -depends Release
 
-task Test -depends Compile, Clean { 
-  $testMessage
+task Test -depends Compile { 
+  exec { . $mstest /testcontainer:"$build_directory\PowerStudio.VsExtension.Tests.dll" }
+}
+
+task IntegrationTest -depends Test { 
+  exec { . $mstest /testcontainer:"$build_directory\PowerStudio.VsExtension.IntegrationTests.dll" }
 }
 
 task Init -depends Clean {
@@ -35,12 +62,12 @@ task Init -depends Clean {
     new-item $build_directory -itemType directory | Out-Null
 }
 
-task Compile -depends Clean {
+task Compile -depends Init {
   exec {msbuild /m:$max_cpu_count /p:BuildInParralel=$build_in_parralel "/logger:FileLogger,Microsoft.Build.Engine;logfile=$msbuild_logfile" /p:Configuration="$build_configuration" /p:Platform="Any CPU" /p:OutDir="$build_directory"\\ "$solution_file"}
 }
 
 task Clean { 
-  #remove-item -force -recurse $build_directory -ErrorAction SilentlyContinue | Out-Null
+  remove-item -force -recurse $build_directory -ErrorAction SilentlyContinue | Out-Null
   remove-item -force -recurse $release_directory -ErrorAction SilentlyContinue | Out-Null
 }
 
