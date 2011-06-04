b72be58298905f1d4134d1ca7cef26d4fff2b8bf
Ian Davis <ian@innovatian.com>
2011-06-04 12:16:12 -0700

    updating assembly info for internalsvisibleto

--- source/PowerStudio.LanguageService/Properties/AssemblyInfo.cs
+++ source/PowerStudio.LanguageService/Properties/AssemblyInfo.cs
@@ -20,3 +20,12 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 
 [assembly : Guid( "f9440583-23e8-49d6-a4e9-d586ca72888b" )]
+
+[assembly:
+        InternalsVisibleTo(
+                "PowerStudio.VsExtension.IntegrationTests, PublicKey=0024000004800000940000000602000000240000525341310004000001000100a91693596e8568c557566c88a02fe5eefdfd6f245c2d5d45e9911357d164cdeb28eae1ccc3ca1795fa4879e206ce309104357940e1697fa50a5bc4aa9a7e3ed9dfcb3ab8cd7c55558612c51844c32b694a3dda142f23f9240456b49d720ccb3beee8cb02ca641beafd7e6a76f1116b426efe89aef0c23634198c9ce5f14ca6bb"
+                )]
+[assembly:
+        InternalsVisibleTo(
+                "PowerStudio.VsExtension.Tests, PublicKey=0024000004800000940000000602000000240000525341310004000001000100a91693596e8568c557566c88a02fe5eefdfd6f245c2d5d45e9911357d164cdeb28eae1ccc3ca1795fa4879e206ce309104357940e1697fa50a5bc4aa9a7e3ed9dfcb3ab8cd7c55558612c51844c32b694a3dda142f23f9240456b49d720ccb3beee8cb02ca641beafd7e6a76f1116b426efe89aef0c23634198c9ce5f14ca6bb"
+                )]
